e4be05e49c393a0dec911cafdb3e5abef4032a43
finchflinch <40954549+finchflinch@users.noreply.github.com>
2019-10-05 12:33:11 +0530

    Add files via upload

---
+++ gsm/gsm.ino
@@ -0,0 +1,36 @@
+int led = 10;
+int smokeA0 = A5;
+// My threshold value
+int sensorThres = 0; //Change this thresold value according to your sensitivity
+
+void setup() {
+  
+  pinMode(led, OUTPUT);
+  pinMode(smokeA0, INPUT);
+  Serial.begin(9600);
+  delay(100);
+}
+
+void loop() {
+  int analogSensor = analogRead(smokeA0);
+  // Checks if it has reached the threshold value
+  if (analogSensor > sensorThres)
+  {
+    
+     Serial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+     delay(1000);  // Delay of 1 second
+     Serial.println("AT+CMGS=\"+917506530870\"\r"); // Replace x with mobile number
+     Serial.println("Gas Leakage Alert");// The SMS text you want to send
+     Serial.println((char)26);// ASCII code of CTRL+Z for saying the end of sms to  the module 
+     delay(100);
+     digitalWrite(led,HIGH);
+     Serial.println("ATD7506530870;"); // ATDxxxxxxxxxx; semicolon should be at the last ;AT command that follows UART protocol;
+
+  }
+  else
+  {
+    
+    digitalWrite(led,LOW);
+  }
+  delay(50);
+}
---
+++ servo/servo.ino
@@ -0,0 +1,63 @@
+char incomingByte;
+int count;
+#include <Servo.h>
+
+Servo myservo;
+
+int pos = 0;
+void setup()
+{
+  Serial.begin(9600);
+  myservo.attach(9);
+}
+
+void loop()
+{
+  if (Serial.available() > 0) 
+  {
+    incomingByte = Serial.read();
+    Serial.print("I got: "); // ASCII printable characters
+    Serial.println(incomingByte, DEC);
+    //count=0;
+    if(incomingByte=='v') //recognised
+    {
+      count=count+5;
+    }
+    if(count%10==0 && count<=20)
+    {
+      for (pos = 0; pos <= 90; pos += 1) 
+      { // goes from 0 degrees to 90 degrees
+  
+        myservo.write(pos);
+        delay(15);
+      }
+  delay(700);
+     for (pos = 90; pos >= 0; pos -= 1)
+      {
+        myservo.write(pos);
+        delay(15);
+      }
+    if(count==20)
+    {
+      exit(0);
+    }
+      
+    }
+    
+    
+    else if(incomingByte=='x')  //not recognised
+    {
+      /*pos=0;
+      myservo.write(pos);
+      delay(15);*/
+      exit(0);
+    }
+    //delay(5000);
+    /*for (pos = 0; pos <= 90; pos += 1) 
+    {
+      myservo.write(pos);
+      delay(15);
+    }*/
+   
+  }
+}
